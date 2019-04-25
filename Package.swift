521a30e786a5153fbe34b8a6d7f1ae04f3d8516e
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-04-25 18:52:42 +0900

    Change Tab to Whitespace(U+0020)

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftHashTest",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "0.9.0")),
-        .package(url: "https://github.com/daisuke-t-jp/FNV.git", .upToNextMinor(from: "1.0.0")),
-        .package(url: "https://github.com/daisuke-t-jp/MurmurHash-Swift.git", .upToNextMinor(from: "1.0.0")),
-        .package(url: "https://github.com/daisuke-t-jp/xxHash-Swift.git", .upToNextMinor(from: "1.0.0")),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftHashTest",
-            dependencies: ["CryptoSwift", "FNV", "MurmurHash-Swift", "xxHash-Swift"]),
-        .testTarget(
-            name: "SwiftHashTestTests",
-            dependencies: ["SwiftHashTest"]),
-    ]
+  name: "SwiftHashTest",
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "0.9.0")),
+    .package(url: "https://github.com/daisuke-t-jp/FNV.git", .upToNextMinor(from: "1.0.0")),
+    .package(url: "https://github.com/daisuke-t-jp/MurmurHash-Swift.git", .upToNextMinor(from: "1.0.0")),
+    .package(url: "https://github.com/daisuke-t-jp/xxHash-Swift.git", .upToNextMinor(from: "1.0.0")),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "SwiftHashTest",
+      dependencies: ["CryptoSwift", "FNV", "MurmurHash-Swift", "xxHash-Swift"]),
+    .testTarget(
+      name: "SwiftHashTestTests",
+      dependencies: ["SwiftHashTest"]),
+  ]
 )
--- Tests/SwiftHashTestTests/XCTestManifests.swift
+++ Tests/SwiftHashTestTests/XCTestManifests.swift
@@ -2,8 +2,8 @@ import XCTest
 
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(SwiftHashTestTests.allTests),
-    ]
+  return [
+      testCase(SwiftHashTestTests.allTests),
+  ]
 }
 #endif
